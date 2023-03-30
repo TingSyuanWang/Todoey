01e634f860785c619575eed12ac5cd425cbca107
TingSyuanWang <vincent@fishboneapps.com>
2023-03-30 19:04:53 -0400

    feat: add UITableView
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["todo1", "todo2", "todo3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 }
-
