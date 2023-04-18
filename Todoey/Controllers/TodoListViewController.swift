2a8f7dd35926ca53ff7cb45f2a2a4e5af18e33a4
TingSyuanWang <vincent@fishboneapps.com>
2023-04-18 16:02:38 -0400

    feat: remove data from Core Data
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -34,7 +34,8 @@ class TodoListViewController: UITableViewController {
     
     // MARK: TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
         saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
