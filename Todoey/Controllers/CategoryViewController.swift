9c9ef46475e8a43436725a716a2d5a129c23c74d
TingSyuanWang <vincent@fishboneapps.com>
2023-04-18 19:41:00 -0400

    feat: setup category view
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Ting-Syuan Wang on 2023/4/18.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Mark: Tableview Datasource Methods
+    
+    // Mark: Data Manipulation Methods
+    
+    // Mark: Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    // Mark: Tableview Delegate Methods
+}
